5ad66bb68859e84a3153bc97ec492620e42c771e
Bolvvv <bolvvv@BolvvvdeMBP.lan>
2019-03-18 23:32:51 +0800

    finish 1

--- src/code121.java
+++ src/code121.java
@@ -1,3 +1,15 @@
 public class code121 {
 
+    //动态规划
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1) return 0;
+        int maxProfile = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i = 1; i < prices.length; i++){
+            if(minPrice > prices[i-1]) minPrice = prices[i-1];
+            int temp = prices[i] - minPrice;
+            maxProfile = maxProfile > temp?maxProfile:temp;
+        }
+        return maxProfile;
+    }
 }
