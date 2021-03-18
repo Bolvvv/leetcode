521d6a8cdfe116a60f80cf2521c80675709ea0cc
bolvvv <bolvvv@bolvvvdeMacBook-Air.local>
2021-03-18 11:31:12 +0800

    完善代码1的方式

--- Cpp/1.cpp
+++ Cpp/1.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
+    //方法1：暴力破解
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+    //方法2：map表
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        int n = nums.size();
+        map<int, int> result;
+        for(int i = 0; i < n; i++){
+            auto iter = result.find(target-nums[i]);
+            if(iter != result.end()){
+                return {i, iter->second};
+            }
+            else{
+                result[nums[i]]=i;
+            }
+        }
+        return {};
     }
 };
--- Cpp/bubble_sort.cpp
+++ Cpp/bubble_sort.cpp
@@ -2,18 +2,30 @@
 // Created by bolvvv on 2021/3/17.
 //
 
-#include "iostream"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void bubble_sort(int a[]){
-    for(int i = 0; i < sizeof(a); i++){
-        for(int j = 0; j < sizeof(a)-1-i; j++){
-            if(a[j]>a[j+1]){
-                int tmp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = tmp;
+vector<int> bubble_sort(vector<int>& nums){
+    vector<int> src_site;
+    for(int i = 0; i < nums.size()-1; i++){
+        for(int j = 0; j < nums.size()-1-i; j++){
+            if(nums[j]>nums[j+1]){
+                int tmp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = tmp;
             }
         }
     }
+    return nums;
+}
+
+int main(){
+    vector<int>a = {1,3,5,2,6,7,9,10,99,1};
+    vector<int>m = bubble_sort(a);
+    for(int i = 0; i < a.size(); i++){
+        cout<<m[i]<<endl;
+    }
+
 }
